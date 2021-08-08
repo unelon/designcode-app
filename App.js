5b6995ab837e4a5fa5e4b8df49df8a2488a3e60f
Rune <rlillevang@gmail.com>
2021-08-08 16:16:03 +0200

    images

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to gdon your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Titlebar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+      <Title>Welcome, back</Title>
+      <Name>Meng</Name>
+      </Titlebar>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+flex: 1;
+background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+font-size: 16px;
+color: #b8bece;
+font-weight:500;
+`;
+
+const Name = styled.Text`
+font-size:20px;
+color: #3c4560;
+font-weight:bold;
+`;
+
+const Titlebar = styled.View`
+width: 100%;
+margin-top: 50px;
+padding-left:80px;
+`;
+
+const Avatar = styled.Image`
+width: 44px;
+height: 44px;
+background: black;
+border-radius: 22px;
+margin-left: 20px;
+position:absolute;
+top: 0;
+left: 0;
+`;
