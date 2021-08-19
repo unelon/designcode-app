de78afd68a311e9d0308182a9a28d2d9c4480c30
Champen <you@example.com>
2021-08-19 23:46:18 +0200

    with reanimated

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 import React from "react";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 import HomeScreen from "./Screens/HomeScreen";
-
-const initialState =  {
-  action: "openMenu"
-}
-
-const reducer = (state = initialState) => {
-  return state;
-};
-
-const store = createStore(reducer)
-
 const App = () => (
-  <Provider store={store}>
-    <HomeScreen />
-  </Provider>
+  <HomeScreen />
 )
 
 export default App;
