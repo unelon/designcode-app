98f09a1d22335befd1f7c46fe2c39a62f21fb0df
Rune <rlillevang@gmail.com>
2021-08-20 13:45:38 +0200

    fix

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import HomeScreen from "./Screens/HomeScreen";
+
+const initialState =  {
+  action: "openMenu"
+}
+
+const reducer = (state = initialState) => {
+  return state;
+};
+
+const store = createStore(reducer)
+
 const App = () => (
-  <HomeScreen />
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
 )
 
 export default App;
